1582b13c628a8f6cfa20dc2e1e86ce62c215ac7f
AchariNagaraj <acharinagaraj19@gmail.com>
2026-03-09 22:53:31 +0530

    Updated switch in main.go- cases: reset, branch , merge

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ func main() {
 			fmt.Println("Usage: minigit add <file>")
 			return
 		}
-
 		err := repository.AddToIndex(os.Args[2])
 		if err != nil {
 			fmt.Println(err)
@@ -39,10 +38,46 @@ func main() {
 			fmt.Println("Usage: minigit commit <message>")
 			return
 		}
-		repository.CreateCommit(os.Args[2])
+		err := repository.CreateCommit(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+		}
 
 	case "log":
-		repository.ShowLog()
+		err := repository.ShowLog()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	case "branch":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: minigit branch <branch-name>")
+			return
+		}
+		err := repository.CreateBranch(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	case "reset":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: minigit reset <commit-hash>")
+			return
+		}
+		err := repository.Reset(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	case "merge":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: minigit merge <branch>")
+			return
+		}
+		err := repository.Merge(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+		}
 
 	default:
 		fmt.Println("Unknown command")
